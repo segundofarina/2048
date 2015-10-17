ee062a7ab1603f71734e55670bb92c0a7461e3fd
Martin Victory <martin@victory.com.ar>
2015-10-16 21:35:00 -0300

    agrego la matriz donde me guarda los casilleros varios

--- sumoFilas.c
+++ sumoFilas.c
@@ -20,9 +20,14 @@ typedef struct {
     int undos;
     int puntaje;
 } tablero;
+typedef struct{
+    //int * matriz[2];
+    int matriz[16][2];
+    int num;
+} casVacios;
 
 
-void sumoFila(movimiento I, movimiento J, tablero m, tablero * nueva){
+void sumoFila(movimiento I, movimiento J, tablero m, tablero * nueva, casVacios * vacios){
 
     int i,j,k=I.inicio, h=J.inicio, sume=0;
 
@@ -51,8 +56,13 @@ void sumoFila(movimiento I, movimiento J, tablero m, tablero * nueva){
             }
         }
     }
+    //vacios->num=0;//inicializo el contador de la matriz que guarda los vacios
     for( ; k!=I.final && h!=J.final; k+=I.incremento, h+=J.incremento ){//relleno con 0 al final de la fila-columna
         nueva->matriz[k][h]=0;
+
+        /*guardo los casilleros vacios*/
+        vacios->matriz[vacios->num][0]=k;
+        vacios->matriz[(vacios->num)++][1]=h;
     }
 }
 
@@ -96,15 +106,17 @@ void descifroMovimiento (int direccion, movimiento * I, movimiento * J,int dim){
 }
 
 
-void muevoTablero(int direccion, tablero viejo, tablero * nuevo){
+void muevoTablero(int direccion, tablero viejo, tablero * nuevo, casVacios * vacios){
     /*Creo los dos sentidos de movimiento y recorro la matriz llamando a la funcion que suma las filas en sentido opuesto a la direccion*/
     movimiento I,J;
 
     descifroMovimiento(direccion,&I,&J,viejo.dim);
 
+    vacios->num=0;//inicializo el contador de la matriz que guarda los vacios
+
     for ( ; I.inicio!=I.final && J.inicio!=J.final; I.inicio+=abs(J.incremento), J.inicio+=abs(I.incremento) )
     {
-        sumoFila(I,J,viejo,nuevo);
+        sumoFila(I,J,viejo,nuevo,vacios);
 
     }
 }
@@ -112,6 +124,7 @@ void muevoTablero(int direccion, tablero viejo, tablero * nuevo){
 int main(){
 
     tablero tablero1={ { {2,2,4,0},{4,2,2,0},{8,4,4,8},{2,2,2,2} }, 4 },tablero2={ {{1,0}}, 4 };
+    casVacios vacios;
 
     int i,j;
 
@@ -126,7 +139,7 @@ int main(){
 
 printf("\n\n**********************\n\n");
 
-    muevoTablero(1,tablero1,&tablero2);
+    muevoTablero(2,tablero1,&tablero2,&vacios);
 
     for(int p=0;p<tablero1.dim;p++){
         for(int o=0;o<tablero1.dim;o++){
@@ -135,6 +148,15 @@ printf("\n\n**********************\n\n");
         printf("\n");
     }
     printf("**** Puntaje: %d\n", tablero2.puntaje);
+
+
+printf("\n\n**********************\n\n");
+
+    printf("I\tJ\n");
+    for(int p=0;p<vacios.num;p++){
+        printf("%d\t%d", vacios.matriz[p][0],vacios.matriz[p][1]);
+        printf("\n");
+    }
 
 
     return 0;
