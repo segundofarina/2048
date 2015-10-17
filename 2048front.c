6ad5c2b8626d496b321cc89448bb48c4cc34688b
segundofarina <segundofarina@MacBook-Pro-de-Segundo.local>
2015-10-17 19:12:06 -0300

    Agrego algo de front

---
+++ 2048front.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include "getnum.h"
+
+typedef struct {
+    int ** matriz;
+   // int matriz[4][4];
+    int dim;
+    int undos;
+    int puntaje;
+} tablero;
+
+int opcion(){
+	printf("********************************\n");
+	printf("*                              *\n");
+	printf("*       Bienvenidos al 2048    *\n");
+	printf("*                              *\n");
+	printf("********************************\n\n");
+	printf("ELIJA UNA OPCION:\n");
+	printf("1-Juego nuevo\n");
+	printf("2-Recuperar un juego guardado\n");
+	printf("3-Salir\n");
+	return getint("");
+}
+int juegoNuevo(){
+	printf("\nELIJA UNA DIFICULTAD:\n");
+	printf("1-Facil\n");
+	printf("2-Intermedio\n");
+	printf("3-Dificil\n");
+	return getint("");
+}
+
+void ImprimirTablero( tablero tablero){
+    int i,j;
+    for(i=0;i<tablero.dim;i++){
+        for(j=0;j<tablero.dim;j++){
+            printf("%d\t", tablero.matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+	if (opcion()==1){
+		juegoNuevo();
+	}
+
+
+}
